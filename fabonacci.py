997603d8ae34eae446df4d0e7ef799e3689e46ed
Yogendra Singh <90251984+Yogendra1213@users.noreply.github.com>
2021-09-28 20:53:51 +0530

    Add files via upload

---
+++ fabonacci.py
@@ -0,0 +1,26 @@
+no=int(input("Enter no, of terms for the Fabonacci series:"))
+n1,n2,=0,1
+count=0
+if no<0:
+    print("Enter a positive Number.")
+elif no==1:
+    print("Your fabonaci series is:",end='\n')
+    print(n1+n2)
+elif no<100:
+    print("Fabonacci series!!")
+    while count<no:
+        print(n1)
+        fab=n1+n2
+        n1=n2
+        n2=fab
+        count +=1
+else:
+    print("Fabonacci series!!")
+    while count<no:
+        print(n1)
+        fab=n1+n2
+        n1=n2
+        n2=fab
+        
+
+
