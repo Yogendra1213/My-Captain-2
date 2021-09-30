5019d1e495052c4a3eeeea2e99fe34dd0e0e8620
Yogendra Singh <90251984+Yogendra1213@users.noreply.github.com>
2021-09-30 15:07:56 +0530

    Update fabonacci.py

--- fabonacci.py
+++ fabonacci.py
@@ -1,4 +1,4 @@
-no=int(input("Enter no, of terms for the Fabonacci series:"))
+no=int(input("Enter no, of terms for the Fabonacci series. Use 0 for an Infinite Fabonacci seies:"))
 n1,n2,=0,1
 count=0
 if no<0:
@@ -6,7 +6,7 @@ if no<0:
 elif no==1:
     print("Your fabonaci series is:",end='\n')
     print(n1+n2)
-elif no<100:
+elif no>1:
     print("Fabonacci series!!")
     while count<no:
         print(n1)
@@ -14,13 +14,13 @@ elif no<100:
         n1=n2
         n2=fab
         count +=1
-else:
+elif no==0:
     print("Fabonacci series!!")
-    while count<no:
+    while True:
         print(n1)
         fab=n1+n2
         n1=n2
         n2=fab
+else:
+    print("Enter a positive value!!!")
         
-
-
